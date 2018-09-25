bd29062c046705dcd580dc49f0d0c38a08dda7d3
Chittapon Thongchim <papcoe@gmail.com>
2018-09-25 21:07:41 +0700

    Initial Commit

---
+++ Network/Network.h
@@ -0,0 +1,19 @@
+//
+//  Network.h
+//  Network
+//
+//  Created by Chittapon Thongchim on 25/9/2561 BE.
+//  Copyright © 2561 Chittapon Thongchim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Network.
+FOUNDATION_EXPORT double NetworkVersionNumber;
+
+//! Project version string for Network.
+FOUNDATION_EXPORT const unsigned char NetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Network/PublicHeader.h>
+
+
